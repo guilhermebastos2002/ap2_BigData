765e094556e089d2c6e4df9b4fa6cc233086dd47
Guilherme Bastos <126486967+guilhermebastos2002@users.noreply.github.com>
2024-11-19 15:31:02 -0300

    Add files via upload

---
+++ ap2_BigData1/ap2_BigData/src/main/java/com/example/ap2_BigData/Ap2BigDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.ap2_BigData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ap2BigDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ap2BigDataApplication.class, args);
+	}
+
+}
---
+++ ap2_BigData1/ap2_BigData/src/test/java/com/example/ap2_BigData/Ap2BigDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ap2_BigData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ap2BigDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
